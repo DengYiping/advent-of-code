09e91be4db75a0f34568071c945d30a09d3d01d1
ydeng <yiping.deng.98@gmail.com>
2024-12-09 22:23:45 +0000

    Add solution for day 9

--- 2024/day9/src/reading.rs
+++ 2024/day9/src/reading.rs
@@ -4,20 +4,20 @@ static ZERO: u8 = '0' as u8;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum DiskSegment {
-    File { id: usize, space: u8 },
-    FreeSpace(u8),
+    DiskFile { id: usize, space: u8 },
+    FreeSpace(u8)
 }
 
-pub fn read() -> Vec<DiskSegment> {
+pub fn read() -> Vec<Vec<DiskSegment>> {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
     let input = input.trim().as_bytes();
-    let mut buffer: Vec<DiskSegment> = vec![];
+    let mut buffer: Vec<Vec<DiskSegment>> = vec![];
     parse(input, 0, true, &mut buffer);
     buffer
 }
 
-pub fn parse(input: &[u8], id: usize, is_file: bool, buffer: &mut Vec<DiskSegment>) {
+pub fn parse(input: &[u8], id: usize, is_file: bool, buffer: &mut Vec<Vec<DiskSegment>>) {
     if input.is_empty() {
         return;
     }
@@ -25,10 +25,10 @@ pub fn parse(input: &[u8], id: usize, is_file: bool, buffer: &mut Vec<DiskSegmen
     let tail = &input[1..];
 
     if is_file {
-        buffer.push(DiskSegment::File { id, space });
+        buffer.push(vec![DiskSegment::DiskFile { id, space }]);
         parse(tail, id + 1, false, buffer);
     } else {
-        buffer.push(DiskSegment::FreeSpace(space));
+        buffer.push(vec![DiskSegment::FreeSpace(space)]);
         parse(tail, id, true, buffer);
     }
 }
